61d0606a7151e151355ca7c00b1533cde58ac1eb
daffaMuttaqin <daffamuttaqinwork@gmail.com>
2026-04-07 11:59:05 +0700

    Perbaiki halaman pdf agar full 1 layar

--- app/Views/sertifikat_view.php
+++ app/Views/sertifikat_view.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sertifikat</title>
+    <link href="/css/output.css" rel="stylesheet">
 </head>
 
-<body>
+<body class="bg-gray-100 m-0">
 
-    <iframe
-        src="<?= base_url('uploads/' . $data['file_pdf']) ?>"
-        width="100%"
-        height="600px">
-    </iframe>
+    <div class="w-full h-screen">
+        <iframe
+            src="<?= base_url('uploads/' . $data['file_pdf']) ?>"
+            class="w-full h-full border-0">
+        </iframe>
+    </div>
 
 </body>
 
